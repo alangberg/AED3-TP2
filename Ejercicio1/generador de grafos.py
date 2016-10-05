66469c95343e47f23a212ac7a80ccfe599aeb211
nicoluce <nicolas.p.luce@gmail.com>
2016-10-05 17:03:29 -0300

    subo generador de grafos para el 1

---
+++ Ejercicio1/generador de grafos.py
@@ -0,0 +1,45 @@
+
+import sys
+import random
+import math
+
+cant_nodos = int(sys.argv[1])
+prob_pared = float(sys.argv[2])
+
+pares_f_c = []
+
+for i in xrange(3, cant_nodos-1):
+	if float(cant_nodos / i).is_integer() and (cant_nodos / i) > 2:
+		pares_f_c.append((i+1, (cant_nodos / i)+1))
+
+par_random = random.choice(pares_f_c)
+F = par_random[0]
+C = par_random[1]
+
+cant_paredes = 0
+
+grafo = [['#'] * C]
+
+for i in xrange(1, F):
+	grafo.append(['#'])
+	for j in xrange(1, C-1):
+		if random.random() <= prob_pared:
+			grafo[i].append('#')
+			cant_paredes += 1
+		else:
+			grafo[i].append('.')
+	grafo[i].append('#')
+
+grafo.append(['#']*C)
+
+grafo[1][1] = 'o'
+grafo[F-1][C-2] = 'x'
+
+P = int(math.ceil(math.sqrt(cant_paredes)))
+
+salida = str(F) + ' ' + str(C) + ' ' + str(P) + '\n'
+for fila in grafo:
+	s = ''.join(fila)
+	salida += s + '\n'
+
+print salida
