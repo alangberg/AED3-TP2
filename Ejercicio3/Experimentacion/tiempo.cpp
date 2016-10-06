4d2f531ad016bf1f296c2096fcc4d09b45bda845
Andres Langberg <andreslangberg@gmail.com>
2016-10-06 11:21:17 -0300

    experimentacion 3 alpha

--- Ejercicio3/Experimentacion/tiempo.cpp
+++ Ejercicio3/Experimentacion/tiempo.cpp
@@ -26,37 +26,32 @@ clock_t tFinish;
 // $ ./tiempo 100 0 33 1 data.out
 int main(int argc, char const *argv[]){
 	int repeticiones = atoi(argv[1]);
-	unsigned long long pMin = atoll(argv[2]);
-	unsigned long long pMax = atoll(argv[3]);
-	int inc = atoi(argv[4]);
 
 	int cantNodos;
 	int cantAristas;
 
 	vector< tupla<int , double> > mediciones;
 
-		cin >> cantNodos >> cantAristas;
+	cin >> cantNodos >> cantAristas;
 
-		Grafo g = Grafo(cantNodos);
+	Grafo g = Grafo(cantNodos);
 
-		g.leer(cantAristas);
+	g.leer(cantAristas);
 
-		vector<int> v;
-		int solucion;
-		double medicion = 0;
-		auto inicio_medicion = ya();
-		for(int i = 0; i < repeticiones; i++){
-			solucion = 	g.caminoMinimo(0,v);
+	vector<int> v;
+	int solucion;
+	double medicion = 0;
+	auto inicio_medicion = ya();
+	for(int i = 0; i < repeticiones; i++){
+		solucion = 	g.caminoMinimo(0,v);
 
-		}
-		auto fin_medicion = ya();
-		tupla<int, double> t(cantNodos, (double) chrono::duration_cast<std::chrono::nanoseconds>(fin_medicion - inicio_medicion).count()/repeticiones);
-		mediciones.push_back(t);
-	
-	std::ofstream salida;
-	salida.open(argv[5]);
+	}
+	auto fin_medicion = ya();
+	tupla<int, double> t(cantNodos, (double) chrono::duration_cast<std::chrono::nanoseconds>(fin_medicion - inicio_medicion).count()/repeticiones);
+	mediciones.push_back(t);
+		
 	for(int i = 0; i < mediciones.size(); i++){
-		salida << mediciones[i].primero << " "
+		cout << mediciones[i].primero << " "
 			<< mediciones[i].segundo << endl;
 	}
 }
